b3b0ab4c8eab8345113d19f1a23e20a2fdebf739
Cassie Schultz <CassieSchultz35@gmail.com>
2021-01-26 13:50:28 -0600

    Add builder call for Cassie's greeting
    This adds both the call for the function, and the method for Cassie's greeting.

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -19,8 +19,10 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(cassieSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
+
 
     return builder.toString();
   }
@@ -31,6 +33,11 @@ public class Hellos {
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String cassieSaysHello(){
+    return "Cassie says 'Hello!'\n";
+
   }
 
 }
