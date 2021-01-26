5c3d702f367f47ae22766620c59acde672a0ef00
Blake Johnson <joh18484@morris.umn.edu>
2021-01-26 13:52:50 -0600

    Added method for Blake's Greeting
    implemented the method blakeSaysHello() and then added a 'builder' call for Blake's Greeting

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -19,8 +19,10 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(blakeSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
+
 
     return builder.toString();
   }
@@ -31,6 +33,10 @@ public class Hellos {
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String blakeSaysHello() {
+    return "Blake says 'What's up!'\n";
   }
 
 }
