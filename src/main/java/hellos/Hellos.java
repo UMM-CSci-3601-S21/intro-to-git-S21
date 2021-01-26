71608013dc047c35073d9c9f7e1da1a862a41158
Joshua Eklund <eklun124@umn.edu>
2021-01-26 13:51:58 -0600

    Add Joshua's greeting.
    This adds Joshua's greeting.

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -19,6 +19,7 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(joshuaSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
 
@@ -31,6 +32,10 @@ public class Hellos {
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String joshuaSaysHello(){
+    return "Joshua says 'Hello!'\n";
   }
 
 }
