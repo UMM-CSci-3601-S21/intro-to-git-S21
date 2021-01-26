8ebbd652f17c6201f087c1e23f887d78174ae9ae
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-01-26 10:52:52 -0900

    Add 'builder' call and implement the method for Isabelle's greeting
    This  adds the 'builder' call and implements a greeting method for Isabelle.

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -19,8 +19,10 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(isabelleSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
+
 
     return builder.toString();
   }
@@ -31,6 +33,10 @@ public class Hellos {
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String isabelleSaysHello() {
+    return "Isabelle says 'Hi!'\n";
   }
 
 }
